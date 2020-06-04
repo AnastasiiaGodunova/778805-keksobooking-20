ca9ccae17af7c88ee8b6a09d0fa47a44c97b1931
AnastasiiaGodunova <icq95@mail.ru>
2020-06-04 16:26:40 +0300

    Округление параметров

--- js/main.js
+++ js/main.js
@@ -42,6 +42,8 @@ var removeClass = function (elem, elemClass) {
 
 /* Возвращает случайное число от и до*/
 var getRandomInteger = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 };
 
